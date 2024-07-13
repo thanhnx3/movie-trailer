3502b2b958668160d172b5174760ed645ff34955
thanhnx3 <ngothanh201100@gmail.com>
2024-07-13 23:24:20 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import PropType from "prop-types";
+import { useState } from "react";
+
+function Header({ onSearch }) {
+  const [textSearch, setSearch] = useState("");
+  return (
+    <div className="p-4 bg-black flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+        <h1 className="text-[40px] uppercase font-bold text-red-700">Movie</h1>
+        <nav className="flex items-center space-x-4">
+          <a href="#" className="text-white">
+            Home
+          </a>
+          <a href="#" className="text-white">
+            About
+          </a>
+          <a href="#" className="text-white">
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-3 text-black"
+          onChange={(e) => setSearch(e.target.value)}
+          value={textSearch}
+        />
+        <button
+          className="p-2 text-white bg-red-600"
+          onClick={() => onSearch(textSearch)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  onSearch: PropType.func,
+};
+
+export default Header;
---
+++ src/components/MovieSearch.jsx
@@ -0,0 +1,47 @@
+import PropType from "prop-types";
+import { useContext } from "react";
+import { MovieContext } from "../context/MovieProvider";
+
+const MovieSearch = ({title, data}) => {
+  const {handleSearch} = useContext(MovieContext)
+  return (
+    <div className="text-white p-10 mb-10">
+      <h2 className="uppercase text-xl font-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        {data &&
+          data.map((item) => (
+            <div
+              key={item.id}
+              className="w-[200px] h-[300px] relative group"
+              onClick={() => handleSearch(item.id)}
+            >
+              <div
+                className="group-hover:scale-105
+              transition-transform duration-500 ease-in-out w-full h-full cursor-pointer"
+              >
+                <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
+                <img
+                  src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
+                  alt={item.title}
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute bottom-4 left-2">
+                  <p className="uppercase text-md">
+                    {item.title || item.original_title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+      
+    </div>
+  );
+};
+
+MovieSearch.propTypes = {
+  title: PropType.string,
+  data: PropType.array,
+};
+
+export default MovieSearch;
